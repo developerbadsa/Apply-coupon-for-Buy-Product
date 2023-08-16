c43a11cad61f7202e6b2ea93d81888d1fc8e18e1
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-08-16 19:54:43 +0600

    product card design finish and js code running

---
+++ assets/app/app.js
@@ -0,0 +1,20 @@
+document.getElementById('couponApply').addEventListener('click', function() {
+    const discount = 30;
+    const inputCoupon = document.getElementById('couponField');
+    const productPrice = Number(document.getElementById('productPrice').innerText);
+    const payBtn = document.getElementById('payBtn');
+
+
+    if (inputCoupon.value === 'd30n') {
+        const discountPrice = productPrice - (productPrice * discount) / 100;
+        payBtn.innerText = discountPrice;
+
+    } else {
+        console.log('ggg')
+    }
+
+    inputCoupon.value = '';
+
+
+
+})
